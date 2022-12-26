c27506e65678e79d1be194302c15f093c1cf015f
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-12-26 14:33:41 +0300

    work

--- Homework 2/main.cpp
+++ Homework 2/main.cpp
@@ -10,6 +10,8 @@ using std::endl;
 #define tab "\t"
 #define delimiter "\n-------------------------------------------------------------------\n"
 
+
+
 const std::map<int, std::string> violation =
 {
 	{0, "Проезд на красный"},
@@ -30,6 +32,7 @@ public:
 		:id(violation), place(place) {}
 	~Crime() {}
 	friend std::ostream& operator<<(std::ostream& os, const Crime& obj);
+	friend void print(std::map<std::string, std::list<Crime>> base);
 };
 
 std::ostream& operator<<(std::ostream& os, const Crime& obj)
@@ -37,15 +40,15 @@ std::ostream& operator<<(std::ostream& os, const Crime& obj)
 	return os << violation.at(obj.id) << " " << obj.place;
 }
 
-template<typename T>void save(int, std::string<T>& place)
-	:id(violation), place(place) {}
+template<typename T>void print(std::map<std::string, std::list<Crime>> base);
+template<typename T>void save(std::map<std::string, std::list<Crime>> base);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
-	std::ofstream fout;               //создали поток
-	fout.open("File_base.txt", std::ios::app);     //открыли
+	//std::ofstream fout;               //создали поток
+	//fout.open("File_base.txt", std::ios::app);     //открыли
 
 	std::map<std::string, std::list<Crime>> base =
 	{
@@ -53,18 +56,49 @@ void main()
 		{"m123ab", {Crime(2, "пл. Революции")}},
 		{"a234bb", {Crime(5, "ул. Ленина"), Crime(4, "ул. Ленина")}},
 	};
+	
+	print(base);
+	//save(base);
+}
+
+template<typename T>void print(std::map<std::string, std::list<Crime>> base)
+{
 	for (std::map<std::string, std::list<Crime>>::iterator it = base.begin(); it != base.end(); ++it)
 	{
-		//cout << it->first << ":\n";
-		fout << it->first << ":\n";
+		cout << it->first << ":\n";
+		//fout << it->first << ":\n";
 		for (std::list<Crime>::iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
 		{
-			//cout << *jt << endl;
-			fout << *jt << endl;
+			cout << *jt << endl;
+			//fout << *jt << endl;
 		}
-		//cout << delimiter << endl;
-		fout << delimiter << endl;
+		cout << delimiter << endl;
+		//fout << delimiter << endl;
 	}
-	fout.close();
-	system("notepad File_base.txt");
 }
+//template<typename T>void save(std::map<std::string, std::list<Crime>> base)
+//{
+//	std::ofstream fout;               //создали поток
+//	fout.open("File_base.txt", std::ios::app);     //открыли
+//	std::map<std::string, std::list<Crime>> base =
+//	{
+//		{"a777bb", {Crime(0, "ул. Ленина"), Crime(6, "ул. Космонавтов"), Crime(3, "ул. Октябрьская")}},
+//		{"m123ab", {Crime(2, "пл. Революции")}},
+//		{"a234bb", {Crime(5, "ул. Ленина"), Crime(4, "ул. Ленина")}},
+//	};
+//
+//	for (std::map<std::string, std::list<Crime>>::iterator it = base.begin(); it != base.end(); ++it)
+//	{
+//		//cout << it->first << ":\n";
+//		fout << it->first << ":\n";
+//		for (std::list<Crime>::iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+//		{
+//			//cout << *jt << endl;
+//			fout << *jt << endl;
+//		}
+//		//cout << delimiter << endl;
+//		fout << delimiter << endl;
+//	}
+//	fout.close();
+//	system("notepad File_base.txt");
+//}
--- Police/main.cpp
+++ Police/main.cpp
@@ -36,23 +36,92 @@ std::ostream& operator<<(std::ostream& os, const Crime& obj)
 {
 	return os << violation.at(obj.id) << " " << obj.place;
 }
+std::ofstream& operator<<(std::ofstream& ofs, const Crime& obj)
+{
+	ofs << obj.id << " " << obj.place;
+	return ofs;
+}
+
+void print(const std::map<std::string, std::list<Crime>> base);
+void save(const std::map<std::string, std::list<Crime>> base, const std::string filename);
+void load(std::map<std::string, std::list<Crime>> base, const std::string& filename);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	std::map<std::string, std::list<Crime>> base =
+	std::map<std::string, std::list<Crime>> base/* =
 	{
 		{"a777bb", {Crime(0, "ул. Ленина"), Crime(6, "ул. Космонавтов"), Crime(3, "ул. Октябрьская")}},
 		{"m123ab", {Crime(2, "пл. Революции")}},
 		{"a234bb", {Crime(5, "ул. Ленина"), Crime(4, "ул. Ленина")}},
 	};
-	for (std::map<std::string, std::list<Crime>>::iterator it = base.begin(); it != base.end(); ++it)
+	print(base);
+	save(base, "base.txt");
+
+
+	system("notepad base.txt")*/;
+
+	load(base, "base.txt");
+	print(base);
+
+}
+
+void print(const std::map<std::string, std::list<Crime>>& base)
+{
+	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
 	{
 		cout << it->first << ":\n";
-		for (std::list<Crime>::iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
 		{
 			cout << *jt << endl;
 		}
 		cout << delimiter << endl;
+	}
+}
+
+void save(const std::map<std::string, std::list<Crime>>& base, const std::string& filename)
+{
+	std::ofstream fout(filename.c_str());
+
+	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+	{
+		fout << it->first << ":\t";
+		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+		{
+			fout << *jt << ",";
+		}
+		fout << endl;
+	}
+	fout.close();
+}
+
+void load(std::map<std::string, std::list<Crime>>& base, const std::string& filename)
+{
+	std::ifstream fin(filename.c_str());
+	if (fin.is_open())
+	{
+		while (!fin.eof())
+		{
+			std::string licence_plate;   //номерной знак
+			std::getline(fin, licence_plate, ':');
+			if (licence_plate.empty())continue;
+			std::string all_crimes;
+			std::getline(fin, all_crimes);
+			all_crimes.erase(0, 1);   //удаляем табуляцию
+			size_t start = 0;
+			size_t end = 0;
+			for (start = 0, end = all_crimes.find(',');
+				end != std::string::npos;
+				start = end, end = all_crimes.find(',', end);
+				)
+			{
+				std::string buffer 
+			}
+		}
+		fin.close();
+	}
+	else
+	{
+		std::cerr << "Error: File not found" << endl;
 	}
 }
